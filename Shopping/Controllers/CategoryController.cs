67aea258fc86f7ec191d229606eca0edd840e97a
Ayman Ahmed <cs.aymanmubark@gmail.com>
2022-10-08 14:07:13 +0300

    ok

---
+++ Shopping/Controllers/CategoryController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Shopping.DTOs;
+using Shopping.IServices;
+using Shopping.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Shopping.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : Controller
+    {
+        private readonly IProductService productService;
+
+        public ProductController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductResponseDTO>>  Get(Guid id)
+        {
+            return await productService.GetProductById(id);
+        }
+
+    }
+}
+
---
+++ Shopping/Services/OrderService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Shopping.Data;
+using Shopping.DTOs;
+using Shopping.Entites;
+using Shopping.IServices;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace Shopping.Services
+{
+    public class ProductService : IProductService
+    {
+        public readonly DataContext _db;
+        public readonly IMapper _mapper;
+        public ProductService(DataContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+
+        public async Task<ProductResponseDTO> GetProductById(Guid id)
+        {
+            Product? product = await _db.Products.Include(x => x.Category)
+                         .Include(x => x.ProductImages)
+                         .Include(x => x.ProductInformations)
+                         .Include(x => x.ProductChoices)
+                         .ThenInclude(x => x.Choice)
+                         .ThenInclude(x => x.ChoiceCategory)
+                         .AsNoTracking()
+                         .SingleOrDefaultAsync(x => x.Id == id);
+            if (product == null)
+            {
+                throw new KeyNotFoundException("product not exist");
+            }
+            return _mapper.Map<ProductResponseDTO>(product);
+        }
+
+        public async Task<List<ProductResponseDTO>> GetProducts()
+        {
+            var result = await _db.Products.Include(x => x.Category)
+                         .Include(x => x.ProductImages)
+                         .Include(x => x.ProductInformations)
+                         .Include(x=>x.ProductChoices)
+                         .ThenInclude(x=>x.Choice)
+                         .ThenInclude(x=>x.ChoiceCategory)
+                         .AsNoTracking()
+                         .ToListAsync();
+
+            return _mapper.Map<List<ProductResponseDTO>>(result);
+        }
+    }
+}
+
